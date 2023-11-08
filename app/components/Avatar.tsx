55bf92a3e695eb310ce88f4f47b70ee8ef9c1b23
snehal gaurkhede <sdgsnehal@gmail.com>
2023-11-08 17:07:28 +0530

    navbar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,8 +1,12 @@
 'use client'
-
+import Image from "next/image"
 const Avatar = () => {
   return (
-    <div>Avatar</div>
+    <Image className="rounded-full"
+    height="30"
+    width="30"
+    alt="Avatar"
+    src="/images/placeholder.png"/>
   )
 }
 
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,7 +1,14 @@
 
 'use client'
 import {AiOutlineMenu} from "react-icons/ai"
+import Avatar from "../Avatar"
+import { useState ,useCallback} from "react"
+import MenuItem from "./MenuItem"
 const UserMenu = () => {
+  const[isOpen,setIsOpen] = useState(false)
+  const toggleMenu =useCallback (() => {
+    setIsOpen((value)=>(!value))
+  },[])
   return (
     <div className='relative'>
         <div className='flex flex-row items-center gap-3'>
@@ -19,7 +26,7 @@ const UserMenu = () => {
             cursor-pointer'>
                 Airbnb your home
             </div>
-            <div onClick={()=>{}}
+            <div onClick={toggleMenu}
             className='p-4
             md:py-1
             md:px-2
@@ -39,6 +46,27 @@ const UserMenu = () => {
              </div>
             </div>
         </div>
+        {isOpen && (
+          <div className="absolute
+          rounded-xl
+          shadow-md
+          w-[40vw]
+          md:w-3/4
+          bg-white
+          overflow-hidden
+          right-0
+          top-12
+          text-sm">
+            <div className="flex flex-col cursor-pointer">
+              <>
+              <MenuItem onClick={()=>{}}
+              label ="login"/>
+              <MenuItem onClick={()=>{}}
+              label ="Sign up"/>
+              </>
+            </div>
+          </div>
+        )}
     </div>
   )
 }
