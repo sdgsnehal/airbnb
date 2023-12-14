5eb4f151ff5fb9c1cc7431d233473b813dfa2a3b
sdgsnehal <sdgsnehal@gmail.com>
2023-12-14 21:11:21 +0530

    imageupload

--- app/components/Inputs/ImageUpload.tsx
+++ app/components/Inputs/ImageUpload.tsx
@@ -1,7 +1,21 @@
 'use client'
 import React from 'react'
+import { CldUploadWidget } from 'next-cloudinary';
+import Image from "next/image"
+import { useCallback } from 'react';
+import {TbPhotoPlus} from "react-icons/tb"
+declare global{
+  var cloudinary:any;
+}
+interface ImaageUploadProps{
+  onChange:(value:string)=>void;
+  value:string
+}
+const ImageUpload:React.FC<ImaageUploadProps> = ({onChange,value}) => {
+  const handleUpload = useCallback((result:any)=>{
+    onChange(result.info.secure_url)
+  },[])
 
-const ImageUpload = () => {
   return (
     <div>ImageUpload</div>
   )
