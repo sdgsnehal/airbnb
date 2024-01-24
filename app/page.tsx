92e398544d8961a16b56aed38e340a5e6231be61
sdgsnehal <sdgsnehal@gmail.com>
2024-01-24 21:51:10 +0530

    dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic' 
 import Container from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";
